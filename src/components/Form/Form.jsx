bd757e814d5012d354eb9767094241c285914da5
iTulinov <energiay@gmail.com>
2022-09-09 16:29:12 +0300

    useInput

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import useInput from '@hooks/useInput.js'
+
 
 const Form = () => {
+    const [titleProps, setTitle] = useInput("");
+    const [colorProps, setColor] = useInput("#000");
+
     return (
         <div>
             <form>
                 <input 
+                    {...titleProps}
                     type="text"
                     placeholder="color title ..."
                     required
                 />
 
                 <input 
+                    {...colorProps}
                     type="text"
                     placeholder="color..."
                     required
