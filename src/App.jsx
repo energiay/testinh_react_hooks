611b8ac65a6a70a2b5b9e765709300690b73423c
iTulinov <energiay@gmail.com>
2022-09-12 19:03:31 +0300

    add compontnt reducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import StarRating from '@components/StarRating/StarRating.jsx'
 import Form from '@components/Form/Form.jsx'
 import Colors from '@components/Colors/Colors.jsx'
 import ColorProvider from '@hooks/useColorProvider.jsx';
+import TestReducer from '@components/TestReducer/TestReducer.js'
 
 const App = () => {
     return (
         <div>
+            <TestReducer />
             <StarRating />
             <hr />
             <Form />
